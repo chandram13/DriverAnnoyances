68e74244b8f3b19583feb3a103332046b6fa420f
chandram13 <mschandr@uci.edu>
2022-08-10 13:31:29 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main() {
+    std::cout << "These are the following scenarios of common driver annoyances." << std::endl;
+
+    void temperature(temp){
+
+    }
+
+
+    void phoneServices(calls, messages){
+
+    }
+
+
+    void drivingSpeed(speed, laneTime){
+
+    }
+
+
+    void highBeams(beamCondition){
+
+    }
+
+    void useGPS(){
+
+    }
+
+    void blockedSituation(route){
+
+    }
+
+
+    void afterAccident(){
+
+    }
+
+
+    void reportAccident(){
+
+    }
+
+
+    void scenarioTailgate(driver){
+
+    }
+
+    return 0;
+
+}
