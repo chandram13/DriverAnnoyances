fb11f8e358627f1e9c13faca167591cfbb901a21
chandram13 <mschandr@uci.edu>
2022-08-10 13:46:39 -0700

    Updated the temperature and phoneservices functions.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@ int main() {
     std::cout << "These are the following scenarios of common driver annoyances." << std::endl;
 
     void temperature(temp){
-
+        temp = int;
+        if temp > 80:
+        cout << "The temperature is above" << temp << "you should turn on the fan or air conditioning."
+        if temp < 65:
+        cout << "The temperature is below" << temp << "you should turn on the heater."
+        else: cout << "Inform the user to look at the car's temperature gauge to determine the current temperature. "
     }
 
 
-    void phoneServices(calls, messages){
+    void phoneServices(calls, message, messages){
+        call = int;
+        messages = int;
+        if call = 1;
+        cout << "Alert the user by putting a notification on the car's screen."
+        cout << message
+        if call > 1 ? messages > 1;
+        cout << "There is more than call or message. Do not alert the driver regarding these calls and/or messages."
+        else:
 
     }
 
@@ -46,5 +59,4 @@ int main() {
     }
 
     return 0;
-
 }
