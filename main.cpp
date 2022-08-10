4f970837d28df47bc5e36b563ea61f001aa3be6e
chandram13 <mschandr@uci.edu>
2022-08-10 14:26:47 -0700

    Updated additional functions

--- main.cpp
+++ main.cpp
@@ -14,35 +14,57 @@ int main() {
 
 
     void phoneServices(calls, message, messages){
-        call = int;
+        calls = int;
         messages = int;
-        if call = 1;
+        if calls = 1;
         cout << "Alert the user by putting a notification on the car's screen."
         cout << message
         if call > 1 ? messages > 1;
         cout << "There is more than call or message. Do not alert the driver regarding these calls and/or messages."
-        else:
-
+        else: cout << "Calls and messages are handled by the user's phone and managed by the automobile's dashboard."
     }
 
 
     void drivingSpeed(speed, laneTime){
+        speed = int;
+        if speed > 85:
+        cout << "The user is going at an excessively high speed, inform the driver to slow down."
+        if laneTime > 5:
+        cout << "Inform the driver to not change lanes since the lane change is too quickly since other drivers may be around."
 
     }
 
 
     void highBeams(beamCondition){
-
+        beamCondition = String;
+        if beamCondition == "completely dark":
+        cout << "The driver should raise their high beams in this scenario."
+        if beamCondition == "foggy":
+        cout << "The driver should raise their high beams to see other cars and drive slowly if cars are in the area."
+        if beamConditon == "raining":
+        cout << "The driver should raise their beams. If there is excessive rain, the driver should raise their high beams."
+        if beamCondition == "snowing":
+        cout << "The driver should purchase white fog lights to see in this condition."
+        else: cout << "The driver should determine if beams need to be used by themselves or the passenger."
     }
 
-    void useGPS(){
-
+    void useGPS(status){
+        status = String;
+        if status == "not driving":
+        cout << "The driver should route their destination if needed using the phone's integrated GPS system or another application."
+        if status == "driving":
+        cout << "The driver should stop at a local rest area or restaurant to navigate to their next destination."
+        else: cout << "The driver should recognize where they are driving or navigate first."
     }
 
-    void blockedSituation(route){
-
+    void blockedSituation(timePassed){
+        timePassed = int; // timePassed in minutes
+        if timePassed == 0:
+        cout << "Alert the driver immediately to avoid the upcoming blocked lane to prevent an accident."
+        if timePassed > 5:
+        cout << "There is a blocked lane ahead, you must skip the exit you want to drive to, and use the following one."
+        else: cout << "The driver should drive carefully and be cautious when driving on the road."
     }
-
 
     void afterAccident(){
 
